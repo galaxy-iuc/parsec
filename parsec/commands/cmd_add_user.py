41017b8202e9f4fc368f5290b1c9fbc10838c4a5
Eric Rasche <rasche.eric@yandex.ru>
2015-02-27 16:18:06 -0600

    Working add_user script

--- parsec/commands/cmd_add_user.py
+++ parsec/commands/cmd_add_user.py
@@ -1,12 +1,18 @@
 import click
 
 from parsec.cli import pass_context
+from parsec.io import error, info
 from parsec import options
-import bioblend
+from bioblend import galaxy
 
 
 @click.command('add_user')
 @options.galaxy_instance()
+
+@click.option(
+    '--username',
+    help="Username for the new user",
+)
 
 @click.option(
     '--email',
@@ -19,11 +25,31 @@ import bioblend
 )
 
 @click.option(
-    '--key',
-    help="API key for the new user",
+    '--remote',
+    is_flag=True,
+    help="This galaxy instance is configured for REMOTE_USER",
 )
 @pass_context
-def cli(ctx, **kwds):
+def cli(ctx, galaxy_instance, username, email, password, remote=False, **kwds):
     """Add a user to a galaxy instance
     """
-    print kwds
+    global_config = ctx.global_config
+    if galaxy_instance not in global_config:
+        # TODO: refactor
+        error("Unknown Galaxy instance, add to ~/.planemo.yml")
+        return -1
+
+    if 'admin' not in global_config[galaxy_instance] or not \
+            global_config[galaxy_instance]['admin']:
+        error("This must be an admin API key (set admin: True in ~/.planemo.yml)")
+        return -2
+
+    gi = galaxy.GalaxyInstance(':'.join([global_config[galaxy_instance]['host'],
+                                         global_config[galaxy_instance]['port']]),
+                               global_config[galaxy_instance]['key'])
+    if remote:
+        result = gi.users.create_remote_user(email)
+    else:
+        result = gi.users.create_local_user(username, email, password)
+
+    info("Created user %(username)s with id %(id)s." % result)
