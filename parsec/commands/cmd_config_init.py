4b18a9fe5454055131202c60f057ffa3372dadbb
Eric Rasche <rasche.eric@yandex.ru>
2015-02-27 16:45:32 -0600

    Allow bootstrapping config with values

--- parsec/commands/cmd_config_init.py
+++ parsec/commands/cmd_config_init.py
@@ -11,11 +11,11 @@ CONFIG_TEMPLATE = """## Parsec Global Configuration File.
 # Each stanza should contian a single galaxy server to control.
 
 local:
-    key: "<TODO>"
+    key: "%(key)s"
     email: "<TODO>"
     password: "<TODO>"
-    host: "127.0.0.1"
-    port: "8080"
+    url: "%(url)s"
+    admin: %(admin)s
 """
 
 SUCCESS_MESSAGE = (
@@ -25,8 +25,22 @@ SUCCESS_MESSAGE = (
 
 
 @click.command("config_init")
+@click.option(
+    '--url',
+    help="URL to galaxy server",
+)
+@click.option(
+    '--api_key',
+    help="API key for galaxy server",
+)
+@click.option(
+    '--admin',
+    is_flag=True,
+    help="This API key is an admin/master API key",
+)
+
 @pass_context
-def cli(ctx, path, **kwds):
+def cli(ctx, url=None, api_key=None, admin=False, **kwds):
     """Help initialize global configuration (in home directory)
     """
     # TODO: prompt for values someday.
@@ -35,5 +49,8 @@ def cli(ctx, path, **kwds):
         warn("File %s already exists, exiting." % config_path)
         return -1
     with open(config_path, "w") as f:
-        f.write(CONFIG_TEMPLATE)
+        f.write(CONFIG_TEMPLATE % {'key': '<TODO>' if api_key is None else api_key,
+                                   'url': '<TODO>' if url is None else url,
+                                   'admin': admin
+                                   })
         info(SUCCESS_MESSAGE % config_path)
