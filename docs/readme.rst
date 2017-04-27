c3888ea2dd6923ba0952855a96456a4ba7e4d8c8
Eric Rasche <rasche.eric@gmail.com>
2017-04-27 17:29:37 +0000

    asdf

--- docs/readme.rst
+++ docs/readme.rst
@@ -1,1 +1,9 @@
 .. include:: ../README.rst
+
+
+Get histories matching a phrase
+-------------------------------
+
+parsec histories_get_histories | \
+    jq '.[] | select(.name | contains("2017.04.27")) | .id'  -r | \
+    xargs parsec histories_update_history --importable true
--- parsec/commands/cmd_histories_update_history.py
+++ parsec/commands/cmd_histories_update_history.py
@@ -16,13 +16,33 @@ from parsec.decorators import bioblend_exception, dict_output
     help="Replace history annotation with given string",
     type=str
 )
+@click.option(
+    "--published",
+    help="Change history published status",
+    type=bool
+)
+@click.option(
+    "--importable",
+    help="Change history importable status",
+    type=bool
+)
 @pass_context
 @bioblend_exception
 @dict_output
-def cli(ctx, history_id, name="", annotation=""):
+def cli(ctx, history_id, name="", annotation="", published=False, importable=False):
     """Update history metadata information. Some of the attributes that can be modified are documented below.
     """
+    kwargs = {}
+    if name and len(name) > 0:
+        kwargs['name'] = name
+    if annotation and len(annotation) > 0:
+        kwargs['annotation'] = annotation
+    if published is not None:
+        kwargs['published'] = published
+    if importable is not None:
+        kwargs['importable'] = importable
+
     return ctx.gi.histories.update_history(
         history_id,
-        name=name,
-        annotation=annotation)
+        **kwargs
+    )
