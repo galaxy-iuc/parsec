f973cbdfff6614e09a0828013355a70280f426d0
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-06-02 07:11:58 +0000

    Fix flake8 problem

--- parsec/config.py
+++ parsec/config.py
@@ -6,17 +6,20 @@ DEFAULT_CONFIG = {
 }
 
 _config_path = os.environ.get(
-        "PARSEC_GLOBAL_CONFIG_PATH",
-        "~/.parsec.yml"
+    "PARSEC_GLOBAL_CONFIG_PATH",
+    "~/.parsec.yml"
 )
 _config_path = os.path.expanduser(_config_path)
-DEFAULT_CONFIG['config_path']=_config_path
+DEFAULT_CONFIG['config_path'] = _config_path
+
 
 def global_config_path():
     return DEFAULT_CONFIG['config_path']
 
+
 def set_global_config_path(config_path):
-    DEFAULT_CONFIG['config_path']=config_path
+    DEFAULT_CONFIG['config_path'] = config_path
+
 
 def read_global_config():
     config_path = global_config_path()
