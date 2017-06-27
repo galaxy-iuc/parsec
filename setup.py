8ebcafac34b5d6df45de4cecc882b129bb604170
E Rasche <hxr@hx42.org>
2017-06-27 11:43:22 +0000

    1.0.3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
     # TODO: put package test requirements here
 ]
 
-version = '1.0.2'
+version = '1.0.3-rc1'
 subpackages = [x.replace('/', '.') for x in glob.glob('parsec/commands/*') if not x.endswith('.py')]
 
 setup(
